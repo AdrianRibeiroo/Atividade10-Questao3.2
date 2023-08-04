65e645ac339a9a83d626209d7cb31b5d2c49d218
Adrian Rubens <132026576+AdrianRibeiroo@users.noreply.github.com>
2023-08-04 07:51:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function compra(a,b)
+{
+    return a-b;
+}
+
+let x = parseInt(prompt('Digite seu saldo:'));
+let y = parseInt(prompt('Digite o valor da compra:'));
+
+if(x>=y)
+{
+    let saldo = compra(x,y);
+    alert(`Compra realizada com sucesso! Saldo atual de ${saldo} reais`);
+}
+else if(x<y)
+{
+    alert(`Não foi possivel realizar a compra, seu saldo é insuficiente! Saldo atual de ${x} reais`);
+}
